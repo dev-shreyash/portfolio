6cc8f3d57e50b3ace0a373c2635724924dc842e6
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-06-13 12:24:27 +0530

    update animations

--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+
+"use client"
+import React,{useEffect} from 'react'
 
 import './main.scss'
+import { gsap } from 'gsap'
+import SplitType from 'split-type'
 const Main = () => {
+  
+useEffect(() => {
+const ourText = new SplitType('span.dev-shreyash', { types: 'chars' })
+const chars = ourText.chars
+
+gsap.fromTo(
+  chars,
+  { 
+    y: 100,
+    opacity: 0
+  },
+  {
+    y: 0,
+    opacity: 1,
+    stagger: 0.2,
+    duration: 5,
+    ease: 'power4.out',
+  }
+)
+}, [])
+
   return (
     <div>
     <main className="bg-black-100 text-white w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
    <div className="name">
-   <span>  dev-Shreyash </span>
+   <span className='dev-shreyash'>  dev-Shreyash </span>
    </div>
    <div className="links">
       <a href=""><img src="/icons8-github.svg" alt="" /></a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import About from "./about/page";
 import Career from "./career/page";
 import Projects from "./projects/page";
 import Contact from "./contact/page";
+import { TsParticles } from "@/components/particles";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
+       <TsParticles/>
        <Navbar/>
        <Main/>
        <About/>

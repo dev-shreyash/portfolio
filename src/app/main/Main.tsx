892550603b039c08584217dad153e12159c6eeb9
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-06-13 17:50:50 +0530

    update homescreen animation

--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -1,53 +1,106 @@
-
 "use client"
-import React,{useEffect} from 'react'
+import React, { useEffect, useRef } from 'react';
+import './main.scss';
+import { gsap } from 'gsap';
+import SplitType from 'split-type';
 
-import './main.scss'
-import { gsap } from 'gsap'
-import SplitType from 'split-type'
 const Main = () => {
-  
-useEffect(() => {
-const ourText = new SplitType('span.dev-shreyash', { types: 'chars' })
-const chars = ourText.chars
+  const buttonsRef = useRef<HTMLDivElement>(null);
+  const iconRef = useRef<HTMLDivElement>(null);
+  const aniText = useRef<HTMLDivElement>(null);
 
-gsap.fromTo(
-  chars,
-  { 
-    y: 100,
-    opacity: 0
-  },
-  {
-    y: 0,
-    opacity: 1,
-    stagger: 0.2,
-    duration: 5,
-    ease: 'power4.out',
-  }
-)
-}, [])
+  useEffect(() => {
+    const ourText = new SplitType('span.dev-shreyash', { types: 'chars' });
+    const chars = ourText.chars;
+    gsap.fromTo(
+      chars,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 5,
+        stagger: 0.03,
+        ease: 'elastic(1.2, 0.5)',
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (buttonsRef.current) {
+      gsap.fromTo(
+        buttonsRef.current,
+        { y: 100, opacity: 0 },
+        { y: 0, opacity: 1, duration: 5, ease: 'power4.out' }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (iconRef.current) {
+      gsap.fromTo(
+        iconRef.current,
+        { y: 100, opacity: 0 },
+        { y: 0, opacity: 1, duration: 5, ease: 'power4.out' }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (aniText.current) {
+      gsap.fromTo(
+        aniText.current,
+        { y: 100, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 5,
+          ease: 'elastic(1.2, 0.5)',
+        }
+      );
+    }
+  }, []);
 
   return (
-    <div>
-    <main className="bg-black-100 text-white w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
-   <div className="name">
-   <span className='dev-shreyash'>  dev-Shreyash </span>
-   </div>
-   <div className="links">
-      <a href=""><img src="/icons8-github.svg" alt="" /></a>
-      <a href=""><img src="/icons8-linkedin.svg" alt="" /></a>
-      <a href=""><img src="/icons8-stack-overflow.svg" alt="" /></a>
-      <a href=""><img src="/icons8-twitterx.svg" alt="" /></a>
-    </div>
-    <div className="buttons">
-      <button className="resume">Resume <img src="/download-svgrepo-com.svg" alt="" /></button>
-      <button className="contact">Contact</button>
-    </div>
-        </main>
- 
-      
-    </div>
-  )
-}
+    <main className="bg-black-100 text-white w-screen h-screen flex flex-col items-center justify-center">
+      <div className="name">
+        <span className="dev-shreyash">dev-Shreyash</span>
+      </div>
+      <div className="bottom">
+        <div className="ani-text" ref={aniText}>
+          <span className="left">Full-Stack Developer</span> | <span className="right">Web Designer</span>
+        </div>
+        <div className="links" ref={iconRef}>
+          <a href=""><img src="/icons8-github.svg" alt="GitHub" /></a>
+          <a href=""><img src="/icons8-linkedin.svg" alt="LinkedIn" /></a>
+          <a href=""><img src="/icons8-stack-overflow.svg" alt="Stack Overflow" /></a>
+          <a href=""><img src="/icons8-twitterx.svg" alt="Twitter" /></a>
+        </div>
+        <div className="buttons" ref={buttonsRef}>
+          <button className="resume">Resume <img src="/download-svgrepo-com.svg" alt="Download" /></button>
+          <button className="contact">Contact</button>
+        </div>
+      </div>
+      <ScrollButton />
+    </main>
+  );
+};
 
-export default Main
+const ScrollButton = () => {
+  const handleScroll = () => {
+    window.scrollBy({
+      top: 1000, // Scroll down by 500 pixels
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="scrollBtn" onClick={handleScroll}>
+      <img src="/scroll-down-svgrepo-com.svg" alt="Scroll Down" />
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap'
 import "./navbar.scss"
 import TransitionLink from "@/components/TransitionLink";
 const Navbar = () => {
+  const lineRef =useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    gsap.fromTo(lineRef.current, 
+      { height: 0 }, 
+      { height: "100%", duration: 2, opacity:1, ease: "power2.out" }
+    );
+   
+  }, []);
   return (
+  
     <div className='nav'>
       {/* <div className="logo"><img src="/spiderman-7810368_1920.png" alt="" /></div> */}
    <div className="left">
+   <div className="verticalLine" ref={lineRef}></div>
     <TransitionLink href="/about" label="About" />
      <TransitionLink href="/career" label="Career" />
      <TransitionLink href="/projects" label="Projects" />      

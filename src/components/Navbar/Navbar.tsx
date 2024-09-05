5e1638a9ec928ee5d034a31f184f65478455a25f
dev-shreyash <bhosaleshreyash2@gmail.com>
2024-09-04 22:08:23 -0700

    update navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -28,15 +28,15 @@ const Navbar = () => {
     <div className='nav'>
       {/* <div className="logo"><img src="/spiderman-7810368_1920.png" alt="" /></div> */}
    <div className="left" ref={fadeIn}>
-   <div className="verticalLine" ref={lineRef}></div>
+   {/* <div className="verticalLine" ref={lineRef}></div> */}
     <TransitionLink href="/about" label="About" />
      <TransitionLink href="/career" label="Career" />
      <TransitionLink href="/projects" label="Projects" />      
      <TransitionLink href="/contact" label="Contact" />
       </div>
-      <div className="right" ref={fadeIn}>
+      {/* <div className="right" ref={fadeIn}>
         <img src="/image.svg" alt="" />
-     </div> 
+     </div>  */}
     </div>
 
   )
